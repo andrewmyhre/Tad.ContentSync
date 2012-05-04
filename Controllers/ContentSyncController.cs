6d6946f6b30be5c9da84b193c6188f4afc0491bf
Andrew Myhre <andrew.myhre@gmail.com>
2012-05-04 20:09:53 +0100

    initial commit, basics working

---
+++ Controllers/ContentSyncController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Core.Common.Models;
+using Orchard.DisplayManagement;
+using Orchard.DisplayManagement.Shapes;
+using Orchard.UI.Admin;
+
+namespace ContentSync.Controllers
+{
+    [Admin]
+    public class ContentSyncController : Controller
+    {
+        private readonly IContentManager _contentManager;
+        private readonly IOrchardServices _services;
+        private readonly IShapeFactory _shapeFactory;
+
+        public ContentSyncController(IContentManager contentManager,
+            IOrchardServices services,
+            IShapeFactory shapeFactory) {
+            _contentManager = contentManager;
+            _services = services;
+            _shapeFactory = shapeFactory;
+        }
+
+        public ActionResult Index() {
+            dynamic Shape = _shapeFactory;
+            var query = _contentManager.Query(VersionOptions.Latest, "Page").List();
+
+            var list = Shape.List();
+            list.AddRange(query.Select(ci => _contentManager.BuildDisplay(ci, "Summary")));
+
+            dynamic viewModel = Shape.ViewModel()
+                .ContentItems(list);
+
+            return View((object)viewModel);
+        }
+    }
+}
---
+++ Routes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace ContentSync
+{
+    public class Routes : IRouteProvider
+    {
+        public IEnumerable<RouteDescriptor> GetRoutes() {
+            return new RouteDescriptor[] {
+                new RouteDescriptor() {
+                    Route=new Route("Admin/ContentSync/{action}",
+                        new RouteValueDictionary{{"area","ContentSync"}, {"action","Index"}, {"controller","ContentSync"}},
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary(){{"area","ContentSync"}},
+                        new MvcRouteHandler())
+                }
+            };
+        }
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes) {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+    }
+}
