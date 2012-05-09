8bdf3b3bff4a60c22d1d0d68e5f113e127fd3801
andrewmyhre <andrew@justgiving.com>
2012-05-09 11:14:33 +0100

    improves content item similarity logic

--- Extensions/ContentItemExtensions.cs
+++ Extensions/ContentItemExtensions.cs
@@ -20,7 +20,14 @@ namespace Tad.ContentSync.Extensions {
                 }
             }
 
-            if (o1.Has<BodyPart>() && o2.Has<BodyPart>()) {
+            if (o1.Has<BodyPart>() && o2.Has<BodyPart>())
+            {
+                var text1 = o1.As<BodyPart>().Text;
+                var text2 = o2.As<BodyPart>().Text;
+
+                if (text1 == null || text2 == null)
+                    return false;
+
                 if (!o1.As<BodyPart>().Text.Equals(o2.As<BodyPart>().Text, StringComparison.CurrentCulture)) {
                     return false;
                 }
@@ -62,20 +69,16 @@ namespace Tad.ContentSync.Extensions {
         }
 
         public static bool SimilarTo(this ContentItem ci1, ContentItem ci2) {
-            if (ci1.ContentType != ci2.ContentType)
-                return false;
-
-            if(ci1.Has<TitlePart>() && ci2.Has<TitlePart>()) {
-                if (!ci1.As<TitlePart>().Title.Equals(ci2.As<TitlePart>().Title, StringComparison.InvariantCultureIgnoreCase))
-                    return false;
-            }
-            /*if (ci1.Has<BodyPart>() && ci2.Has<BodyPart>())
+            if (ci1.ContentType.Equals(ci2.ContentType))
             {
-                if (!ci1.As<BodyPart>().Text.Equals(ci2.As<BodyPart>().Text, StringComparison.InvariantCultureIgnoreCase))
-                    return false;
-            }*/
+                if (ci1.Has<TitlePart>() && ci2.Has<TitlePart>())
+                {
+                    if (ci1.As<TitlePart>().Title.Equals(ci2.As<TitlePart>().Title, StringComparison.InvariantCultureIgnoreCase))
+                        return true;
+                }
+            }
 
-            return true;
+            return false;
         }
     }
 }
